2564ffa4c651313cc66ab115c5a5e46063717036
Chamon Sheikh <sheikhchamon9@gmail.comm>
2024-07-07 23:37:15 +0600

    problems solved

---
+++ practiceProblem.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int price;
+    scanf("%d", &price);
+    // if (price >= 10000)
+    // {
+    //     printf("Gucci Bag\n");
+    //     if(price> 20000)
+    //     {
+    //         printf("Gucci Bet");
+    //     }
+    // }
+    // else if (price >= 5000)
+    // {
+    //     printf("Levis Bag");
+    // }
+    // else
+    // {
+    //     printf("Something");
+    // }
+    for(int i=0; i>=0; i++)
+    {
+        printf("h\n");
+    }
+    return 0;
+}
